91f7108e6f53141abc087cae734de097c805d736
yusran <jhyyusran766@gmail.com>
2025-09-06 16:35:20 +0800

    Add files via upload

---
+++ Days6.java
@@ -0,0 +1,11 @@
+
+package pkg100dayscoding;
+public class Days6 {
+    public static void main(String[] args) {
+    String nama;
+    int angka;
+    boolean JavaIsFun;
+    double BeratBadan;
+    char Inisial;
+   }
+ }
